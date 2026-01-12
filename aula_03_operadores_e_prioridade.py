feefcb5aa8adf97c4c414a453c4f61cc7beda711
Tpp0329 <tpprox2016@gmail.com>
2026-01-12 11:53:04 -0300

    operator precedence examples

--- aula_03_operadores_e_prioridade.py
+++ aula_03_operadores_e_prioridade.py
@@ -1,4 +1,11 @@
-# --- PRECEDÊNCIA DE OPERADORES ---
-# A potência (**) manda mais que o sinal de menos (-)
-print(-3 ** 2)   # Resultado: -9 (O Python faz 3**2 primeiro)
-print((-3) ** 2) # Resultado: 9  (O parênteses muda a ordem)
+# --- ESTUDO DE PRECEDÊNCIA (Quem manda mais?) ---
+
+# A potência (**) tem prioridade sobre o sinal de negativo (-)
+print(-3 ** 2)    # Resultado: -9 (O Python faz 3**2 primeiro)
+
+# Usando parênteses para mudar a prioridade
+print((-3) ** 2)  # Resultado: 9 (O Python eleva o número negativo ao quadrado)
+
+# Outro exemplo: Multiplicação vs Soma
+print(10 + 2 * 5)   # Resultado: 20 (Faz 2*5 primeiro)
+print((10 + 2) * 5) # Resultado: 60 (Faz a soma primeiro por causa dos parênteses)
