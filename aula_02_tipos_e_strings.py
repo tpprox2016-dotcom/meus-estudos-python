4b03be633cc52c1c7debcee331bf37d147201064
Tpp0329 <tpprox2016@gmail.com>
2026-01-12 11:31:18 -0300

    Add integer division examples in aula_02_tipos_e_strings.py

---
+++ aula_02_tipos_e_strings.py
@@ -0,0 +1,6 @@
+# --- DIVISÃO INTEIRA (//) ---
+# O Python arredonda sempre para baixo (o menor valor na reta numérica)
+print(-6 // 4)  # Dá -2 (porque -2 é menor que -1.5)
+
+# Se houver um float na conta, o resultado vira float
+print(6. // -4) # Dá -2.0
