db1433f69d64361178a0c2324ca0c6fd2c2bb93c
Tpp0329 <tpprox2016@gmail.com>
2026-01-12 11:52:10 -0300

    Add operator precedence examples in aula_03_operadores_e_prioridade.py

---
+++ aula_03_operadores_e_prioridade.py
@@ -0,0 +1,4 @@
+# --- PRECEDÊNCIA DE OPERADORES ---
+# A potência (**) manda mais que o sinal de menos (-)
+print(-3 ** 2)   # Resultado: -9 (O Python faz 3**2 primeiro)
+print((-3) ** 2) # Resultado: 9  (O parênteses muda a ordem)
