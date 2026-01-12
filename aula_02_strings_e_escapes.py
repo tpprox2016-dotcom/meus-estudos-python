3cda4d776776008df73e6590c1da0322c7d63e83
Tpp0329 <tpprox2016@gmail.com>
2026-01-12 10:16:50 -0300

    Add examples of string handling in Python

---
+++ aula_02_strings_e_escapes.py
@@ -0,0 +1,9 @@
+# --- TRABALHANDO COM STRINGS EM PYTHON ---
+
+# Opção 1: Usando Caracteres de Escape (\)
+# Útil para colocar aspas dentro de aspas sem quebrar o código
+print("Estudando \"Python\" com escapes")
+
+# Opção 2: Usando Aspas Triplas
+# Jeito mais fácil e limpo de escrever textos complexos
+print("""Estudando "Python" de forma muito mais simples""")
