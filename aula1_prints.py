f53dbbb21ecd4b06c4d35ec227c9f4dea562d423
Tpp0329 <tpprox2016@gmail.com>
2026-01-12 09:56:41 -0300

    Update aula1_prints.py

--- aula1_prints.py
+++ aula1_prints.py
@@ -1,5 +1,8 @@
-print("sacola", "trabalho", "vermelho", sep=",",end=" ")
+# Aula 01 - Praticando Print e Separadores
+print("sacola", "trabalho", "vermelho", sep=",", end=" ")
 print("estádio", end=".")
+
+# Praticando quebra de linha e novos separadores
 print("estudo", "garrafa", "azul", sep="-") 
 print("abc")
 print("leite")
