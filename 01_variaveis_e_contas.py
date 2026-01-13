a488226b3c684680f1820a616c37af7370032d74
Tpp0329 <tpprox2016@gmail.com>
2026-01-13 11:45:23 -0300

    Add initial variable operations and print statements

---
+++ 01_variaveis_e_contas.py
@@ -0,0 +1,12 @@
+# Meus primeiros passos com variáveis
+# Aprendi que o valor pode ser sobrescrito
+
+var = 100
+print("Valor inicial:", var)
+
+var = 200 + 300
+print("Valor após a soma:", var)
+
+# Usando o valor anterior para criar um novo
+total = var * 2
+print("O dobro de var é:", total)
